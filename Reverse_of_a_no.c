82e5681c02e621e6350db1b30601c57b5aa6d311
Ankit verma <92791509+ankit2003A@users.noreply.github.com>
2022-05-30 23:16:06 +0530

    Create Reverse_of_a_no.c

---
+++ Reverse_of_a_no.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main(){
+    int a,sum=0;
+    printf("enter number = ");
+    scanf("%d",&a);
+    while(a>0){
+        sum=sum*10+(a%10);
+        a=a/10;
+    }
+    printf("%d",sum);
+}
