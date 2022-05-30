76d8a78707dfb8386aee149f20e4b005ffb924c8
Ankit verma <92791509+ankit2003A@users.noreply.github.com>
2022-05-30 23:28:16 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main(){
+    int a,sum=0,num;
+    printf("enter number = ");
+    scanf("%d",&a);
+    num=a;
+    while(a>0){
+        sum=sum*10+(a%10);
+        a=a/10;
+    }
+    printf("%d\n",sum);
+    if(sum==num){
+        printf("palindrome");
+    }
+    else{
+        printf("Not palindrome");
+    }
+}
